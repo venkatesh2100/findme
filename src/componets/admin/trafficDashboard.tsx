7cb9391ffdb31bd5452c91c4ffd6cb0ecbdbc069
Vignesh1002 <vigneshviki1005@gmail.com>
2025-11-10 22:01:25 -0600

    usa choloropeth map done

--- src/componets/admin/trafficDashboard.tsx
+++ src/componets/admin/trafficDashboard.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import ChloroplethMap from "./chloropethMap";
 const COLORS = ["#009FFD", "#8CDEFC", "#D3F4FF"];
 const returningColors = ["#009FFD", "#D3F4FF"];
 
@@ -237,49 +238,9 @@ const TrafficDashboard = () => {
         </select>
       </div>
       {/* Country Map Section */}
-      <div className="bg-white p-4 rounded-xl shadow-sm space-y-4 mt-10">
-        <div className="border-2 flex border-[#D2D6F9] rounded-xl overflow-hidden">
-          <div className="w-full h-[600px]">
-            <div className="ml-4 mt-2 text-sm font-bold">
-              Session by United States
-            </div>
-            <Image
-              src="/USA.png"
-              alt="USA Map"
-              width={600}
-              height={600}
-              className="object-cover w-full h-full"
-            />
-          </div>
-          <div className=" p-4 rounded-xl pr-10 space-y-4">
-            {/* Country Selector */}
-            <div className="flex items-center gap-2 text-gray-700 text-sm font-medium">
-              <ChevronLeft size={16} />
-              <span>USA</span>
-            </div>
-
-            {/* States Sessions */}
-            <div className="space-y-4">
-              {stateSessions.map(({ state, value }) => (
-                <div key={state}>
-                  <div className="flex justify-between items-center text-sm font-semibold text-gray-800">
-                    <div className="flex items-center gap-1">
-                      {state} <ChevronRight size={14} />
-                    </div>
-                    <span>{value}</span>
-                  </div>
-                  <div className="w-full h-2 bg-gray-200 rounded-full mt-1">
-                    <div
-                      className="h-2 bg-blue-400 rounded-full"
-                      style={{ width: `${(value / maxSessions) * 100}%` }}
-                    />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+      <div>
+        <ChloroplethMap />
+      </div> 
     </div>
   );
 };
